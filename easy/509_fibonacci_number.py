2215e41e34832744fd5470a38b505f5dfb9fb88c
PeterChen <sanpeter992000@gmail.com>
2021-12-05 21:20:49 +0800

    add memoization in recursive solution

--- easy/509_fibonacci_number.py
+++ easy/509_fibonacci_number.py
@@ -1,29 +1,33 @@
 # recursive solution
+def fib(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        memo[n] = fib(n - 2, memo) + fib(n - 1, memo)
+    return memo[n]
+
+
+# non-recursive solution
 # def fib(n):
 #     if n == 0:
 #         return 0
 #     elif n == 1:
 #         return 1
 #     else:
-#         return fib(n - 2) + fib(n - 1)
-
-# non-recursive solution
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        pre_one = -1
-        pre_two = 1
-        result = 0
-        for i in range(n + 1):
-            result = pre_one + pre_two
-            pre_one = pre_two
-            pre_two = result
-        return result
+#         pre_one = -1
+#         pre_two = 1
+#         result = 0
+#         for i in range(n + 1):
+#             result = pre_one + pre_two
+#             pre_one = pre_two
+#             pre_two = result
+#         return result
 
 
 if __name__ == '__main__':
-    n = 5
+    n = 50
     print(fib(n))

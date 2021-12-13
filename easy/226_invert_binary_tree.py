6831fc4e7cf6c437d8e6d0efa405c962b9f15af9
PeterChen <sanpeter992000@gmail.com>
2021-12-13 21:31:14 +0800

    Easy 226. Invert Binary Tree

---
+++ easy/226_invert_binary_tree.py
@@ -0,0 +1,33 @@
+# Tree
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return None
+
+        def helper(node):
+            node.left, node.right = node.right, node.left
+            if node.left:
+                helper(node.left)
+            if node.right:
+                helper(node.right)
+
+        helper(node=root)
+        return root
+
+
+if __name__ == '__main__':
+    root = TreeNode(val=4,
+                    left=TreeNode(val=2,
+                                  left=TreeNode(val=1),
+                                  right=TreeNode(val=3)),
+                    right=TreeNode(val=7,
+                                   left=TreeNode(val=6),
+                                   right=TreeNode(val=9)))
+    print(Solution().invertTree(root=root))

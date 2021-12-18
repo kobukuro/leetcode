20426439c0d448b7de054f54c15efdf305269b00
PeterChen <sanpeter992000@gmail.com>
2021-12-19 00:43:40 +0800

    377. Combination Sum IV

---
+++ Dynamic Programming/377_combination_sum_4.py
@@ -0,0 +1,21 @@
+# Dynamic Programming
+class Solution:
+    def combinationSum4(self, nums, target, memo={}):
+        if target in memo:
+            return memo[target]
+        if target == 0:
+            return 1
+        if target < 0:
+            return 0
+        total = 0
+        for num in nums:
+            remainder = target - num
+            total += self.combinationSum4(nums=nums, target=remainder, memo=memo)
+        memo[target] = total
+        return memo[target]
+
+
+if __name__ == '__main__':
+    nums = [9]
+    target = 3
+    print(Solution().combinationSum4(nums=nums, target=target))

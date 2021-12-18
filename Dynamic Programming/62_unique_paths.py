b9a40bdd9f91c5f891944735668844b3058902c9
PeterChen <sanpeter992000@gmail.com>
2021-12-18 16:45:25 +0800

    62. Unique Paths

---
+++ Dynamic Programming/62_unique_paths.py
@@ -0,0 +1,16 @@
+# Dynamic Programming
+class Solution:
+    def uniquePaths(self, m, n, memo={}):
+        key = (m, n)
+        if key in memo:
+            return memo[key]
+        if m == 1 or n == 1:
+            return 1
+        if m == 0 or n == 0:
+            return 0
+        memo[key] = self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        return memo[key]
+
+
+if __name__ == '__main__':
+    print(Solution().uniquePaths(m=7, n=3))

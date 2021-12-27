26a9302875551b2c58c67814b56872154abb8fdf
PeterChen <sanpeter992000@gmail.com>
2021-12-27 08:41:27 +0800

    268. Missing Number

---
+++ Array/268. Missing Number.py
@@ -0,0 +1,13 @@
+# Array
+class Solution:
+    def missingNumber(self, nums):
+        ans = 0
+        for i in range(len(nums) + 1):
+            if i not in nums:
+                ans = i
+        return ans
+
+
+if __name__ == '__main__':
+    nums = [3, 0, 1]
+    print(Solution().missingNumber(nums=nums))

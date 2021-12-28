a1b458818cd5ab12106c20039b2d62b7b74318bf
PeterChen <sanpeter992000@gmail.com>
2021-12-28 08:04:04 +0800

    299. Bulls and Cows

---
+++ String/299. Bulls and Cows.py
@@ -0,0 +1,26 @@
+# String
+from collections import defaultdict
+
+
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        bull = 0
+        cow = 0
+        s = defaultdict(int)
+        g = defaultdict(int)
+        for i in range(len(guess)):
+            if guess[i] == secret[i]:
+                bull += 1
+            else:
+                s[secret[i]] += 1
+                g[guess[i]] += 1
+        for key in s.keys():
+            if key in g:
+                cow += min(s[key], g[key])
+        return f'{bull}A{cow}B'
+
+
+if __name__ == '__main__':
+    secret = '1807'
+    guess = '7810'
+    print(Solution().getHint(secret=secret, guess=guess))

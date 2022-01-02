c57dc17d89338884a3bda948ac3aa56e68240bb8
PeterChen <sanpeter992000@gmail.com>
2022-01-02 11:03:11 +0800

    229. Majority Element II

---
+++ Array/229. Majority Element II.py
@@ -0,0 +1,32 @@
+# Array
+from collections import defaultdict
+
+
+class Solution:
+    def majorityElement(self, nums):
+        if len(nums) == 1:
+            return nums
+        i = 0
+        j = len(nums) - 1
+        count = defaultdict(int)
+        ans = []
+        while i <= j:
+            if i != j:
+                count[nums[i]] += 1
+                count[nums[j]] += 1
+                if count[nums[i]] > len(nums) / 3 and nums[i] not in ans:
+                    ans.append(nums[i])
+                if count[nums[j]] > len(nums) / 3 and nums[j] not in ans:
+                    ans.append(nums[j])
+            else:
+                count[nums[i]] += 1
+                if count[nums[i]] > len(nums) / 3 and nums[i] not in ans:
+                    ans.append(nums[i])
+            i += 1
+            j -= 1
+        return ans
+
+
+if __name__ == '__main__':
+    nums = [3, 3, 4]
+    print(Solution().majorityElement(nums=nums))

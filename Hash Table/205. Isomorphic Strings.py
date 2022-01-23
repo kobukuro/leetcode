3ac9e43988e24d2a830d180d3bfc636640912391
PeterChen <sanpeter992000@gmail.com>
2022-01-23 08:55:11 +0800

    205. Isomorphic Strings

---
+++ Hash Table/205. Isomorphic Strings.py
@@ -0,0 +1,26 @@
+# Hash Map, String
+# need to be improved
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        i = 0
+        hash_map = {}
+        if len(s) != len(t):
+            return False
+        while i < len(s):
+            if s[i] not in hash_map and t[i] not in hash_map.values():
+                hash_map[s[i]] = t[i]
+            elif s[i] in hash_map and t[i] not in hash_map.values():
+                return False
+            elif s[i] not in hash_map and t[i] in hash_map.values():
+                return False
+            else:
+                if hash_map[s[i]] != t[i]:
+                    return False
+            i += 1
+        return True
+
+
+if __name__ == '__main__':
+    s = "bcb"
+    t = "bcc"
+    print(Solution().isIsomorphic(s=s, t=t))

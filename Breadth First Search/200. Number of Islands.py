dcbe9daafb1f363e975a0905364b9f06c15df6f4
PeterChen <sanpeter992000@gmail.com>
2022-01-28 00:05:33 +0800

    200. Number of Islands

---
+++ Breadth First Search/200. Number of Islands.py
@@ -0,0 +1,43 @@
+# Array, Depth-First Search, Breadth-First Search, Union Find, Matrix
+# reference:https://www.youtube.com/watch?v=pV2kpPD66nE
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r, c):
+            visited.add((r, c))
+            q = deque()
+            q.append((r, c))
+            while q:
+                row, col = q.popleft()
+                directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if r in range(rows) and c in range(cols) and grid[r][c] == '1' and (r, c) not in visited:
+                        q.append((r, c))
+                        visited.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+        return islands
+
+
+if __name__ == '__main__':
+    grid = [
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"]
+    ]
+    print(Solution().numIslands(grid=grid))

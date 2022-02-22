afd05d5c78526c53d3f8838a1b7fb52708e2dede
PeterChen <sanpeter992000@gmail.com>
2022-02-22 14:06:11 +0800

    provide my slow solution

--- Math/171. Excel Sheet Column Number.py
+++ Math/171. Excel Sheet Column Number.py
@@ -1,5 +1,22 @@
 # Math, String
-# 背起來
+import math
+
+
+# slow
+# class Solution:
+#     # A  -> 1
+#     # AA -> 26*1 + 1 = 27
+#     # AAA-> 26*26*1 + 26*1 + 1 = 703
+#     def titleToNumber(self, columnTitle: str) -> int:
+#         result = 0
+#         digit_num = 0
+#         for i in range(len(columnTitle) - 1, -1, -1):
+#             # print(columnTitle[i])
+#             result += int(math.pow(26, digit_num)) * (ord(columnTitle[i]) - ord('A') + 1)
+#             digit_num += 1
+#         return result
+
+# fast
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         result = 0
@@ -10,4 +27,4 @@ class Solution:
 
 
 if __name__ == '__main__':
-    print(Solution().titleToNumber(columnTitle='BA'))
+    print(Solution().titleToNumber(columnTitle='AAA'))

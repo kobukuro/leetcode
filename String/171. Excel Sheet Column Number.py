9616eef74199a3de86ea0e49ba90b48afe262cc4
PeterChen <sanpeter992000@gmail.com>
2022-02-22 08:58:03 +0800

    171. Excel Sheet Column Number

---
+++ String/171. Excel Sheet Column Number.py
@@ -0,0 +1,13 @@
+# Math, String
+# 背起來
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        for c in columnTitle:
+            d = ord(c) - ord('A') + 1
+            result = result * 26 + d
+        return result
+
+
+if __name__ == '__main__':
+    print(Solution().titleToNumber(columnTitle='AAA'))

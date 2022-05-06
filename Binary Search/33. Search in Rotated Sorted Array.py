2ef17888f13da5cd0ded74fbfacc5d5b589b0d63
PeterChen <sanpeter992000@gmail.com>
2022-05-07 00:36:16 +0800

    modified code

--- Binary Search/33. Search in Rotated Sorted Array.py
+++ Binary Search/33. Search in Rotated Sorted Array.py
@@ -1,42 +1,39 @@
 # Array, Binary Search
+# Time:O(log n), Space:O(1)
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def binary_search(nums, target):
-            left = 0
-            right = len(nums) - 1
+        # Time:O(log n), Space:O(1)
+        def binary_search(left, right):
             while left <= right:
-                pivot = (left + right) // 2
-                if nums[pivot] == target:
-                    return pivot
-                elif nums[pivot] < target:
-                    left = pivot + 1
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    right = pivot - 1
+                    left = mid + 1
             return -1
-
-        def find_rotation_index(nums, target):
+        def find_rotation_index():
             left = 0
             right = len(nums) - 1
             while left <= right:
-                pivot = (left + right) // 2
-                if nums[pivot] > nums[pivot + 1]:
-                    return pivot
-                if nums[pivot] >= nums[left]:
-                    left = pivot + 1
+                mid = left + (right - left) // 2
+                if nums[mid] > nums[mid+1]:
+                    return mid + 1
+                if nums[mid] < nums[mid-1]:
+                    return mid
+                if nums[mid] > nums[0]:
+                    left = mid + 1
                 else:
-                    right = pivot - 1
-
-        if nums[-1] >= nums[0]:
-            return binary_search(nums, target)
-        rotation_index = find_rotation_index(nums, target)
-        # print(rotation_index)
-        if target == nums[rotation_index]:
-            return rotation_index
-        if target >= nums[0]:
-            return binary_search(nums[:rotation_index + 1], target)
+                    right = mid - 1
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        if nums[-1] > nums[0]:
+            return binary_search(0, len(nums)-1)
+        rotation_index = find_rotation_index()
+        if target == nums[0]:
+            return 0
+        elif target > nums[0]:
+            return binary_search(0, rotation_index-1)
         else:
-            index = binary_search(nums[rotation_index + 1:], target)
-            if index == -1:
-                return -1
-            else:
-                return rotation_index + 1 + index
+            return binary_search(rotation_index, len(nums)-1)

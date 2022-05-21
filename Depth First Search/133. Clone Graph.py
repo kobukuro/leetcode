53172d619035dbc0bb62748baa210d6aaf2567da
PeterChen <sanpeter992000@gmail.com>
2022-05-21 17:20:02 +0800

    add complexity analysis description

--- Depth First Search/133. Clone Graph.py
+++ Depth First Search/133. Clone Graph.py
@@ -1,5 +1,13 @@
 # Hash Table, Depth-First Search, Breadth-First Search, Graph
 # reference:https://www.youtube.com/watch?v=mQeF6bN8hMk
+
+# Time Complexity: O(N + M), where N is a number of nodes (vertices) and M is a number of edges.
+# Space Complexity: O(N). This space is occupied by the visited hash map
+# and in addition to that, space would also be occupied by the recursion stack
+# since we are adopting a recursive approach here.
+# The space occupied by the recursion stack would be equal to O(H)
+# where H is the height of the graph. Overall, the space complexity would be O(N).
+
 class Node:
     def __init__(self, val=0, neighbors=None):
         self.val = val

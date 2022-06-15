34e0ffca97b78ca37a75b11cc867ff5335d15828
PeterChen <sanpeter992000@gmail.com>
2022-06-15 22:08:03 +0800

    add complexity analysis comment and tabulation approach

--- Dynamic Programming/70. Climbing Stairs.py
+++ Dynamic Programming/70. Climbing Stairs.py
@@ -1,5 +1,12 @@
 # Math, Dynamic Programming, Memoization
-class Solution:
+
+# Brute Force
+# Time:O(2^n), 因為每個節點都會有兩個分支, 然後樹的高度為n
+# Space:O(n). The depth of the recursion tree can go upto n.
+
+# Time:O(n), O(2n) -> O(n)
+# Space:O(n)
+class MemoizationSolution:
     def climbStairs(self, n: int, memo=None) -> int:
         if memo is None:
             memo = {}
@@ -11,3 +18,15 @@ class Solution:
             return 0
         memo[n] = self.climbStairs(n - 1, memo) + self.climbStairs(n - 2, memo)
         return memo[n]
+
+
+# Time:O(n)
+# Space:O(n)
+class TabulationSolution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]

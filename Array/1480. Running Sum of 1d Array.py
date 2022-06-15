427b28d5a6389938dd7d7649fca3b3a23680fd28
PeterChen <sanpeter992000@gmail.com>
2022-06-15 08:59:30 +0800

    1480. Running Sum of 1d Array

---
+++ Array/1480. Running Sum of 1d Array.py
@@ -0,0 +1,9 @@
+# Array, Prefix Sum
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        res = []
+        curr = 0
+        for num in nums:
+            curr += num
+            res.append(curr)
+        return res

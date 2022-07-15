172f50d457ee2490bba9f89e48db82957b8429a9
PeterChen <sanpeter992000@gmail.com>
2022-07-15 20:44:49 +0800

    tabulation solution of 198. House Robber

--- Dynamic Programming/198. House Robber.py
+++ Dynamic Programming/198. House Robber.py
@@ -27,3 +27,27 @@ class MemoizationSolution:
             return memo[curr_index]
 
         return dfs(0, nums)
+
+
+# Time Complexity: O(N) since we have a loop from 0...len(nums)-1
+# and we simply use the pre-calculated values of our dynamic programming table
+# for calculating the current value in the table which is a constant time operation.
+# Space Complexity: O(N) which is used by the table.
+# So what is the real advantage of this solution over the previous solution?
+# In this case, we don't have a recursion stack.
+# When the number of houses is large, a recursion stack can become a serious limitation,
+# because the recursion stack size will be huge
+# and the compiler will eventually run into stack-overflow problems (no pun intended!).
+class TabulationSolution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+        # print(dp)
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[-1]

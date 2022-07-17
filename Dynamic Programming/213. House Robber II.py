b470aaae302874b40a912c3e14f251396f779027
PeterChen <sanpeter992000@gmail.com>
2022-07-17 10:58:12 +0800

    add tabulation solution and complexity analysis in 213. House Robber II

--- Dynamic Programming/213. House Robber II.py
+++ Dynamic Programming/213. House Robber II.py
@@ -2,6 +2,8 @@
 from typing import List
 
 
+# Time Complexity: O(N)
+# Space Complexity: O(N)
 class MemoizationSolution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
@@ -20,3 +22,25 @@ class MemoizationSolution:
             return memo[curr_index]
 
         return max(dfs(0, nums[:-1]), dfs(0, nums[1:]))
+
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+class TabulationSolution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        def rob_dp(nums):
+            if len(nums) == 1:
+                return nums[0]
+            if len(nums) == 2:
+                return max(nums[0], nums[1])
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+            for i in range(2, len(nums)):
+                dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+            return dp[-1]
+
+        return max(rob_dp(nums[:-1]), rob_dp(nums[1:]))

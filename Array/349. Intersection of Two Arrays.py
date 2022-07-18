ef35a7ee32b63221d00401598220d06d70e4fb20
PeterChen <sanpeter992000@gmail.com>
2022-07-18 20:22:51 +0800

    349. Intersection of Two Arrays

---
+++ Array/349. Intersection of Two Arrays.py
@@ -0,0 +1,18 @@
+# Array, Hash Table, Two Pointers, Binary Search, Sorting
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        i = 0
+        j = 0
+        res = set()
+        while i != len(nums1) and j != len(nums2):
+            if nums1[i] > nums2[j]:
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                res.add(nums1[i])
+                i += 1
+                j += 1
+        return list(res)

2d072793a11f8150ccf3f1ca01cbae2151504027
PeterChen <sanpeter992000@gmail.com>
2022-07-30 21:31:19 +0800

    add comment and modified code

--- Dynamic Programming/139. Word Break.py
+++ Dynamic Programming/139. Word Break.py
@@ -4,8 +4,13 @@ from typing import List
 
 # n is the length of the input string.
 # Time complexity : O(n^3).
-# first n is the recursion, second n is the for loop, and third n is due to substring
+# first n is the recursion (we keep track of how many times word break is being called),
+# second n is the for loop
+# (we keep track of how many for loop iterations each one of those calls is making),
+# and third n is due to substring.
 # Space complexity : O(n). The depth of recursion tree can go up to n.
+# we already have a cache size of N
+# and the recursive stack in the worst case is always going to go at a maximum of size N.
 class MemoizationSolution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         def dfs(s, set_words, start, memo=None):
@@ -36,7 +41,7 @@ class TabulationSolution:
         dp[-1] = True
         for start in range(len(s) - 1, -1, -1):
             for word in wordDict:
-                if s[start:start + len(word)] == word and start + len(word) <= len(s):
+                if start + len(word) <= len(s) and s[start:start + len(word)] == word:
                     dp[start] = dp[start + len(word)]
                 if dp[start]:
                     break

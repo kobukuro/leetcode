15272779c17e3ef0f2bd2f061cfd18419e403791
PeterChen <sanpeter992000@gmail.com>
2022-08-01 19:16:20 +0800

    use backtracking solution instead

--- Dynamic Programming/39. Combination Sum.py
+++ Dynamic Programming/39. Combination Sum.py
@@ -4,20 +4,33 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
 
-        def dfs(sum, numbers, number, path):
-            # print(f'{sum}-{number}={sum-number}')
-            sum -= number
-            if sum == 0:
-                path.sort()
-                if path not in res:
-                    res.append(path)
-            if sum < 0:
+        results = []
+
+        def backtrack(remain, comb, start):
+            print(candidates[start], comb)
+            if remain == 0:
+                # make a deep copy of the current combination
+                results.append(list(comb))
                 return
-            for num in numbers:
-                dfs(sum, numbers, num, path=path + [num])
+            elif remain < 0:
+                # exceed the scope, stop exploration.
+                return
 
-        for number in candidates:
-            dfs(target, candidates, number, path=[number])
-        return res
+            for i in range(start, len(candidates)):
+                # add the number into the combination
+                comb.append(candidates[i])
+                # give the current number another chance, rather than moving on
+                backtrack(remain - candidates[i], comb, i)
+                # backtrack, remove the number from the combination
+                comb.pop()
+
+        backtrack(target, [], 0)
+
+        return results
+
+
+if __name__ == '__main__':
+    candidates = [3, 4, 5]
+    target = 8
+    Solution().combinationSum(candidates, target)

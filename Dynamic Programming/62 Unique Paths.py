4d59e841de50716098e124e1540728a76444f41a
PeterChen <sanpeter992000@gmail.com>
2022-08-09 20:35:42 +0800

    add tabulation solution and complexity analysis

--- Dynamic Programming/62 Unique Paths.py
+++ Dynamic Programming/62 Unique Paths.py
@@ -1,18 +1,54 @@
 # Dynamic Programming, Math, Combinatorics
-class Solution:
-    def uniquePaths(self, m, n, memo=None):
-        if memo is None:
-            memo = {}
-        key = (m, n)
-        if key in memo:
-            return memo[key]
-        if m == 1 or n == 1:
-            return 1
+# reference:https://www.youtube.com/watch?v=oBt53YbR9Kk&t=2319s
+
+# Time Limit Exceeded
+# Time:O(2^(m+n))
+# Space:O(m+n)
+class BruteForceSolution:
+    def uniquePaths(self, m, n):
         if m == 0 or n == 0:
             return 0
-        memo[key] = self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        if m == 1 and n == 1:
+            return 1
+        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+
+
+# Time:O(m*n)
+# Space:O(m+n)
+class MemoizationSolution:
+    def uniquePaths(self, m, n, memo=None):
+        key = (m, n)
+        if memo is None:
+            memo = {}
+        if m == 0 or n == 0:
+            return 0
+        if m == 1 and n == 1:
+            return 1
+        if key in memo:
+            return memo[key]
+        memo[key] = self.uniquePaths(m - 1, n, memo) + self.uniquePaths(m, n - 1, memo)
         return memo[key]
 
 
+# Time:O(m*n)
+# Space:O(m*n)
+class TabulationSolution:
+    def uniquePaths(self, m, n):
+        dp = []
+        for i in range(m + 1):
+            row = []
+            for j in range(n + 1):
+                row.append(0)
+            dp.append(row)
+        dp[1][1] = 1
+        for i in range(m + 1):
+            for j in range(n + 1):
+                if i + 1 <= m:
+                    dp[i + 1][j] += dp[i][j]
+                if j + 1 <= n:
+                    dp[i][j + 1] += dp[i][j]
+        return dp[-1][-1]
+
+
 if __name__ == '__main__':
-    print(Solution().uniquePaths(m=7, n=3))
+    print(MemoizationSolution().uniquePaths(m=18, n=18))

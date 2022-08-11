60ad7d008a11388e1f6002e37502c36fa1cca8fe
PeterChen <sanpeter992000@gmail.com>
2022-08-11 19:15:44 +0800

    add complexity analysis

--- Dynamic Programming/91. Decode Ways.py
+++ Dynamic Programming/91. Decode Ways.py
@@ -1,11 +1,20 @@
 # String, Dynamic Programming
+
+# n is the length of the input string.
+# Time complexity : O(n^3).
+# first n is the recursion (we keep track of how many times word break is being called),
+# second n is the for loop
+# (we keep track of how many for loop iterations each one of those calls is making),
+# and third n is due to substring.
+# Space complexity : O(n). The depth of recursion tree can go up to n.
+# we already have a cache size of N
+# and the recursive stack in the worst case is always going to go at a maximum of size N.
 class MemoizationSolution:
     def numDecodings(self, s: str) -> int:
         candidates = []
         for i in range(1, 27):
             candidates.append(str(i))
 
-        # print(candidates)
         def dfs(start, memo=None):
             if memo is None:
                 memo = {}
@@ -23,13 +32,17 @@ class MemoizationSolution:
         return dfs(0)
 
 
+# n is the length of the input string.
+# Time complexity : O(n^3).
+# There are two nested loops, and substring computation at each iteration.
+# Overall that results in O(n^3) time complexity.
+# Space complexity : O(n). Length of dp array is n+1.
 class TabulationSolution:
     def numDecodings(self, s: str) -> int:
         candidates = []
         for i in range(1, 27):
             candidates.append(str(i))
         dp = [0] * (len(s) + 1)
-        # print(dp)
         dp[-1] = 1
         for start in range(len(s) - 1, -1, -1):
             for candidate in candidates:

37fbf7afd3c5e511d497c4ac6a8ed348fb3b537a
PeterChen <sanpeter992000@gmail.com>
2022-10-25 20:56:42 +0800

    1832. Check if the Sentence Is Pangram

---
+++ String/1832. Check if the Sentence Is Pangram.py
@@ -0,0 +1,7 @@
+# String, Hash Table
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        for i in range(97, 123):
+            if chr(i) not in sentence:
+                return False
+        return True

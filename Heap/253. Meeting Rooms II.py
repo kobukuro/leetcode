d27c8a48bc16368a69f0ec2f659360ae7c6261e8
PeterChen <sanpeter992000@gmail.com>
2022-12-11 09:23:51 +0800

    add complexity analysis

--- Heap/253. Meeting Rooms II.py
+++ Heap/253. Meeting Rooms II.py
@@ -3,6 +3,13 @@ from typing import List
 import heapq
 
 
+# Time Complexity: O(NlogN).
+# There are two major portions that take up time here.
+# One is sorting of the array that takes O(NlogN) considering that the array consists of N elements.
+# Then we have the min-heap. In any case we have N add operations on the heap.
+# Overall complexity being (NlogN) since ADD operation on a heap takes O(logN).
+# Space Complexity: O(N) because we construct the min-heap and that can contain N elements
+# when N meetings will collide with each other.
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort()

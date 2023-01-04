5defcbe24f6170c45c5423860b595cede6e598a9
PeterChen <sanpeter992000@gmail.com>
2023-01-04 18:58:59 +0800

    647. Palindromic Substrings

---
+++ String/647. Palindromic Substrings.py
@@ -0,0 +1,18 @@
+# String, Dynamic Programming
+# Time complexity : O(n^2)
+# Space complexity : O(1).
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        for i in range(len(s)):
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+        return res

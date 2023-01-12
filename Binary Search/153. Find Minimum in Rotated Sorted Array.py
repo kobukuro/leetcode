985410f2e4cf23cfd13ee5705f8c924a34a5becf
PeterChen <sanpeter992000@gmail.com>
2023-01-12 19:29:27 +0800

    change solution and add complexity analysis

--- Binary Search/153. Find Minimum in Rotated Sorted Array.py
+++ Binary Search/153. Find Minimum in Rotated Sorted Array.py
@@ -1,16 +1,24 @@
 # Array, Binary Search
+from typing import List
+
+
+# Time complexity:O(log n)
+# Space complexity:O(1)
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
         left = 0
         right = len(nums) - 1
-        res = nums[0]
+        if nums[right] > nums[left]:
+            return nums[0]
         while left <= right:
-            print(left, right)
-            pivot = (left + right) // 2
-            print(pivot)
-            res = min(res, nums[pivot], nums[left])
-            if nums[pivot] >= nums[left]:
-                left = pivot + 1
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            if nums[mid] < nums[mid - 1]:
+                return nums[mid]
+            if nums[0] < nums[mid]:
+                left = mid + 1
             else:
-                right = pivot - 1
-        return res
+                right = mid - 1

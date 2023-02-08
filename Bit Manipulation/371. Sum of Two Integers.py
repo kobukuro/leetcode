ff0ba3abb7f85f95ffd8f0281bb17d839583480a
PeterChen <sanpeter992000@gmail.com>
2023-02-08 19:21:57 +0800

    371. Sum of Two Integers

---
+++ Bit Manipulation/371. Sum of Two Integers.py
@@ -0,0 +1,33 @@
+# Math, Bit Manipulation
+# Time complexity: O(1)
+# Space complexity: O(1)
+class Solution:
+    # 1011(11) + 1001(9) = 10100(20)
+    # 10010(18) - 00110(6) = 1100(12)
+    # 42 - 27 = 15
+    def getSum(self, a: int, b: int) -> int:
+        x, y = abs(a), abs(b)
+        # ensure that abs(a) >= abs(b)
+        if x < y:
+            return self.getSum(b, a)
+
+        # abs(a) >= abs(b) -->
+        # a determines the sign
+        sign = 1 if a > 0 else -1
+
+        if a * b >= 0:
+            # sum of two positive integers x + y
+            # where x > y
+            while y:
+                answer = x ^ y
+                carry = (x & y) << 1
+                x, y = answer, carry
+        else:
+            # difference of two integers x - y
+            # where x > y
+            while y:
+                answer = x ^ y
+                borrow = ((~x) & y) << 1
+                x, y = answer, borrow
+
+        return x * sign

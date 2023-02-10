c269cbddc922af51384725f506fa5b078b93a7e5
PeterChen <sanpeter992000@gmail.com>
2023-02-11 06:13:11 +0800

    338. Counting Bits

--- Bit Manipulation/338. Counting Bits.py
+++ Bit Manipulation/338. Counting Bits.py
@@ -2,7 +2,23 @@
 from typing import List
 
 
+# Time complexity: O(n).
+# For each integer x, in the range 1 to n,
+# we need to perform a constant number of operations which does not depend on the number of bits in x.
+# Space complexity: O(1).
+# Since the output array does not count towards the space complexity.
 class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
+
+
+class SlowSolution:
     def countBits(self, n: int) -> List[int]:
         ans = []
         for i in range(n + 1):

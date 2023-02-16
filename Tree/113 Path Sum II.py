c6c46b0719e5efa5d1a0ee16369de3d53e6507bb
PeterChen <sanpeter992000@gmail.com>
2023-02-16 19:31:00 +0800

    provide another solution

--- Tree/113 Path Sum II.py
+++ Tree/113 Path Sum II.py
@@ -1,4 +1,7 @@
 # Tree, Backtracking, Depth-First Search, Binary Tree
+from typing import Optional, List
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -6,7 +9,37 @@ class TreeNode:
         self.right = right
 
 
+# Time Complexity: O(N^2)
+# where N are the number of nodes in a tree.
+# In the worst case, we could have a complete binary tree and if that is the case,
+# then there would be N/2 leaves. For every leaf, we perform a potential
+# O(N) operation of copying over the path nodes to a new list to be added to the final res.
+# Hence, the complexity in the worst case could be O(N^2).
+
+# Space Complexity: O(N)
+# The only additional space that we use is the path list to keep track of nodes along a branch.
 class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        res = []
+
+        def dfs(node, path, remaining_sum):
+            if node:
+                path.append(node.val)
+                if not node.left and not node.right and remaining_sum == node.val:
+                    res.append(list(path))
+                if node.left:
+                    dfs(node.left, path, remaining_sum - node.val)
+                if node.right:
+                    dfs(node.right, path, remaining_sum - node.val)
+                path.pop()
+
+        if root:
+            dfs(root, [], targetSum)
+        return res
+
+
+# https://stackoverflow.com/questions/11177533/whats-the-difference-between-plus-and-append-in-python-for-list-manipulation
+class AnotherSolution:
 
     def pathSum(self, root, targetSum):
         all_paths = []
@@ -19,6 +52,7 @@ class Solution:
                 helper(root.left, path + [root.left.val], total + root.left.val)
             if root.right:
                 helper(root.right, path + [root.right.val], total + root.right.val)
+
         if root:
             helper(root, [root.val], root.val)
         return all_paths

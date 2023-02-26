0a818859197547526bb910aaa58d1cca308649c1
PeterChen <sanpeter992000@gmail.com>
2023-02-26 10:27:37 +0800

    faster solution

--- Sliding Window/424. Longest Repeating Character Replacement.py
+++ Sliding Window/424. Longest Repeating Character Replacement.py
@@ -24,3 +24,28 @@ class Solution:
                 left += 1
             res = max(res, right - left + 1)
         return res
+
+
+# At any given window,
+# max_freq will only be violated if the start index happens to be pointing at the char with max frequency.
+# For example: "AABBA", start = 0, end = 4.
+# If we shrink the window by moving the start pointer to the right by 1, max_freq should be 2 instead of 3.
+# Keep in mind that the way we validate the window is by comparing windowLength - max_freq with k.
+# When the situation described earlier happens,
+# notice that the most frequent char is removed from both the max_freq count as well as the windowLength count.
+# In other words "the number of chars that need to be replaced" becomes (windowLength - 1) - (max_freq - 1).
+# The two -1s cancel out.
+class FasterSolution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hm = defaultdict(int)
+        l = 0
+        res = 0
+        max_freq = 0
+        for r in range(len(s)):
+            hm[s[r]] += 1
+            max_freq = max(max_freq, hm[s[r]])
+            while (r - l + 1) - max_freq > k:
+                hm[s[l]] -= 1
+                l += 1
+            res = max(res, (r - l + 1))
+        return res

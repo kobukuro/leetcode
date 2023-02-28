d9ae43b8bba8cbdc382ab6d97f5226f739d1d701
PeterChen <sanpeter992000@gmail.com>
2023-02-28 08:14:45 +0800

    add complexity analysis

--- Trie/208. Implement Trie (Prefix Tree).py
+++ Trie/208. Implement Trie (Prefix Tree).py
@@ -5,6 +5,15 @@ class Trie:
         self.links = {}
         self.is_end = is_end
 
+    # Time complexity : O(m), where m is the key length.
+    # In each iteration of the algorithm,
+    # we either examine or create a node in the trie till we reach the end of the key.
+    # This takes only m operations.
+
+    # Space complexity : O(m).
+    # In the worst case newly inserted key doesn't share
+    # a prefix with the the keys already inserted in the trie.
+    # We have to add m new nodes, which takes us O(m) space.
     def insert(self, word: str) -> None:
         curr_node = self
         for w in word:
@@ -16,6 +25,11 @@ class Trie:
                 curr_node = curr_node.links[w]
         curr_node.is_end = True
 
+    # Time complexity : O(m)
+    # In each step of the algorithm we search for the next key character.
+    # In the worst case the algorithm performs m operations.
+
+    # Space complexity : O(1)
     def search(self, word: str) -> bool:
         curr_node = self
         for w in word:
@@ -25,6 +39,8 @@ class Trie:
                 curr_node = curr_node.links[w]
         return curr_node.is_end
 
+    # Time complexity : O(m)
+    # Space complexity : O(1)
     def startsWith(self, prefix: str) -> bool:
         curr_node = self
         for w in prefix:

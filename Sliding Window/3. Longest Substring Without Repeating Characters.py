6c2c1984ab7eda687eb6f790ac4da660121c7975
PeterChen <sanpeter992000@gmail.com>
2023-03-01 19:10:28 +0800

    simplify code

--- Sliding Window/3. Longest Substring Without Repeating Characters.py
+++ Sliding Window/3. Longest Substring Without Repeating Characters.py
@@ -1,5 +1,5 @@
 # Sliding Window, Hash Table, String
-# Time complexity : O(2n) = O(n). In the worst case each character will be visited twice by i and j,
+# Time complexity : O(2n) = O(n). In the worst case each character will be visited twice by l and r,
 # Space:O(n)
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
@@ -7,10 +7,9 @@ class Solution:
         l = 0
         res = 0
         for r in range(len(s)):
-            if s[r] in hash_set:
-                while s[r] in hash_set:
-                    hash_set.remove(s[l])
-                    l += 1
+            while s[r] in hash_set:
+                hash_set.remove(s[l])
+                l += 1
             hash_set.add(s[r])
             res = max(res, r - l + 1)
         return res

baf6964ade04cf27468c68adcaa52b2f93574713
PeterChen <sanpeter992000@gmail.com>
2023-04-08 11:14:28 +0800

    Dynamic Programming Solution

--- Dynamic Programming/53. Maximum Subarray.py
+++ Dynamic Programming/53. Maximum Subarray.py
@@ -1,4 +1,7 @@
 # Array, Divide and Conquer, Dynamic Programming
+from typing import List
+
+
 # Time:O(n), Space:O(1)
 class Solution:
     def maxSubArray(self, nums):
@@ -9,6 +12,16 @@ class Solution:
             curr_sum += nums[i]
             res = max(res, curr_sum)
         return res
+
+
+# Time:O(n), Space:O(n)
+class DPSolution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], nums[i] + dp[i - 1])
+        return max(dp)
 
 
 if __name__ == '__main__':

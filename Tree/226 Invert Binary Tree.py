995cf052a054e6cbdd60abeb3d83e5e0cd31e11f
PeterChen <sanpeter992000@gmail.com>
2023-05-20 18:10:50 +0800

    simplify code

--- Tree/226 Invert Binary Tree.py
+++ Tree/226 Invert Binary Tree.py
@@ -14,10 +14,8 @@ class Solution:
         if not root:
             return root
         root.left, root.right = root.right, root.left
-        if root.left:
-            self.invertTree(root.left)
-        if root.right:
-            self.invertTree(root.right)
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
 
 

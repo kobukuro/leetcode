9d267be3f57e6fc3808bb72fcb7a17b110aa19c1
PeterChen <sanpeter992000@gmail.com>
2023-07-07 19:52:35 +0800

    modified code

--- Tree/700. Search in a Binary Search Tree.py
+++ Tree/700. Search in a Binary Search Tree.py
@@ -10,14 +10,7 @@ class Solution:
         if not root:
             return None
         if root.val == val:
-            # print(root.val)
             return root
-        if root.left:
-            result = self.searchBST(root.left, val)
-            if result:
-                return result
-        if root.right:
-            result = self.searchBST(root.right, val)
-            if result:
-                return result
-        return None
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        return self.searchBST(root.right, val)

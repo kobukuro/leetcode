5685fbc3cf5309a4f5e49cf4821d00f2c7ea9ce9
PeterChen <sanpeter992000@gmail.com>
2023-09-08 21:27:56 +0800

    simplify code

--- Hash Table/383. Ransom Note.py
+++ Hash Table/383. Ransom Note.py
@@ -1,24 +1,16 @@
 # Hash Table, String, Counting
+from collections import defaultdict
+
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        r_hash_map = {}
-        for ran in ransomNote:
-            if ran not in r_hash_map:
-                r_hash_map[ran] = 1
-            else:
-                r_hash_map[ran] += 1
-        m_hash_map = {}
-        for ma in magazine:
-            if ma not in m_hash_map:
-                m_hash_map[ma] = 1
-            else:
-                m_hash_map[ma] += 1
-        print(r_hash_map)
-        print(m_hash_map)
-        for key, value in r_hash_map.items():
-            if key in m_hash_map:
-                if value > m_hash_map[key]:
-                    return False
-            else:
+        count_r = defaultdict(int)
+        for c in ransomNote:
+            count_r[c] += 1
+        count_m = defaultdict(int)
+        for c in magazine:
+            count_m[c] += 1
+        for key, value in count_r.items():
+            if count_m[key] < value:
                 return False
         return True

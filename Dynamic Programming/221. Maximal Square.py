ada7dafefe85f5881c956d0ee3011d6ddebee3db
Peter Chen <sanpeter992000@gmail.com>
2025-06-07 11:03:17 +0800

    Add solution for "221. Maximal Square" using dynamic programming.

---
+++ Dynamic Programming/221. Maximal Square.py
@@ -0,0 +1,28 @@
+# Array, Dynamic Programming, Matrix
+from typing import List
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        max_edge = 0
+        ROWS, COLS = len(matrix), len(matrix[0])
+        dp = []
+        for r in range(ROWS + 1):
+            row = []
+            for c in range(COLS + 1):
+                row.append(0)
+            dp.append(row)
+        for r in range(1, ROWS + 1):
+            for c in range(1, COLS + 1):
+                if matrix[r - 1][c - 1] == '1':
+                    dp[r][c] = min(min(dp[r - 1][c], dp[r][c - 1]), dp[r - 1][c - 1]) + 1
+                    max_edge = max(max_edge, dp[r][c])
+        return max_edge * max_edge
+
+
+if __name__ == "__main__":
+    matrix = [["1", "0", "1", "0", "0"],
+              ["1", "0", "1", "1", "1"],
+              ["1", "1", "1", "1", "1"],
+              ["1", "0", "0", "1", "0"]]
+    print(Solution().maximalSquare(matrix))  # Output: 4

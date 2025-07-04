e4315ee88711453d92bfe6347439eee9e7820491
Peter Chen <sanpeter992000@gmail.com>
2025-07-04 17:47:39 +0800

    Refactor contains_duplicate test assertions for clarity.

--- problems/easy/contains_duplicate_217/test_solution.py
+++ problems/easy/contains_duplicate_217/test_solution.py
@@ -7,12 +7,12 @@ class TestSolution:
 
     def test_example_1(self):
         nums = [1, 2, 3, 1]
-        assert self.solution.contains_duplicate(nums) is True
+        assert self.solution.contains_duplicate(nums)
 
     def test_example_2(self):
         nums = [1, 2, 3, 4]
-        assert self.solution.contains_duplicate(nums) is False
+        assert not self.solution.contains_duplicate(nums)
 
     def test_example_3(self):
         nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
-        assert self.solution.contains_duplicate(nums) is True
+        assert self.solution.contains_duplicate(nums)

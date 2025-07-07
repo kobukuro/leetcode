55e3451c5f2015150ce9ef5adf10caf1486bd773
Peter Chen <sanpeter992000@gmail.com>
2025-07-07 12:28:20 +0800

    Fix parameterization formatting in test for "Container With Most Water" solution.

--- problems/medium/container_with_most_water_11/test_solution.py
+++ problems/medium/container_with_most_water_11/test_solution.py
@@ -6,7 +6,7 @@ from .solution import Solution
 class TestSolution(BaseTestSolution):
     solution = Solution()
 
-    @pytest.mark.parametrize("method_name, height , expected, timeout", [
+    @pytest.mark.parametrize("method_name, height, expected, timeout", [
         ('max_area', [1, 8, 6, 2, 5, 4, 8, 3, 7], 49, None),
         ('max_area', [1, 1], 1, None),
     ])

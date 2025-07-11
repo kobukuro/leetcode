8348028b17bd2c566da1c59c391af84b90f74e18
Peter Chen <sanpeter992000@gmail.com>
2025-07-11 12:04:06 +0800

    Add solution for "Unique Paths" problem with memoization and tabulation methods, including unit tests.

---
+++ problems/medium/unique_paths_62/test_solution.py
@@ -0,0 +1,18 @@
+import pytest
+from tests.base_test import BaseTestSolution
+from .solution import Solution
+
+
+class TestSolution(BaseTestSolution):
+    solution = Solution()
+
+    @pytest.mark.parametrize("method_name, m, n, expected, timeout", [
+        ('unique_paths_memoization', 3, 7, 28, None),
+        ('unique_paths_memoization', 3, 2, 3, None),
+        ('unique_paths_memoization', 23, 12, 193536720, BaseTestSolution.DEFAULT_TIMEOUT),
+        ('unique_paths_tabulation', 3, 7, 28, None),
+        ('unique_paths_tabulation', 3, 2, 3, None),
+        ('unique_paths_tabulation', 23, 12, 193536720, BaseTestSolution.DEFAULT_TIMEOUT),
+    ])
+    def test_rob(self, method_name, m, n, expected, timeout):
+        self._run_test(self.solution, method_name, (m, n,), expected, timeout)

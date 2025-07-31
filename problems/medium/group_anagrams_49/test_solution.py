a6ba1cc0dd4bfec5d2a745291c6599f0a4ea127c
Peter Chen <sanpeter992000@gmail.com>
2025-07-31 11:17:27 +0800

    Add helper method to assert anagram groups equality in tests.

--- problems/medium/group_anagrams_49/test_solution.py
+++ problems/medium/group_anagrams_49/test_solution.py
@@ -5,15 +5,16 @@ class TestSolution:
     def setup_method(self):
         self.solution = Solution()
 
+    def assert_anagram_groups_equal(self, result, expected):
+        result_sets = [set(group) for group in result]
+        expected_sets = [set(group) for group in expected]
+        assert set(map(frozenset, result_sets)) == set(map(frozenset, expected_sets))
+
     def test_example_1_sorted_string(self):
         strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
         expected = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
         result = self.solution.group_anagrams_sorted_string(strs)
-
-        result_sets = [set(group) for group in result]
-        expected_sets = [set(group) for group in expected]
-
-        assert set(map(frozenset, result_sets)) == set(map(frozenset, expected_sets))
+        self.assert_anagram_groups_equal(result, expected)
 
     def test_example_2_sorted_string(self):
         strs = [""]
@@ -30,12 +31,8 @@ class TestSolution:
     def test_example_1_count(self):
         strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
         expected = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
-        result = self.solution.group_anagrams_count(strs)
-
-        result_sets = [set(group) for group in result]
-        expected_sets = [set(group) for group in expected]
-
-        assert set(map(frozenset, result_sets)) == set(map(frozenset, expected_sets))
+        result = self.solution.group_anagrams_sorted_string(strs)
+        self.assert_anagram_groups_equal(result, expected)
 
     def test_example_2_count(self):
         strs = [""]

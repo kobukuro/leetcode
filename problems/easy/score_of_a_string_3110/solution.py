0fbd48a9682d078de8c1e1c6ef4a1be1fab74b45
Peter Chen <sanpeter992000@gmail.com>
2025-08-02 11:17:39 +0800

    Use hyphens instead in the comment.

--- problems/easy/score_of_a_string_3110/solution.py
+++ problems/easy/score_of_a_string_3110/solution.py
@@ -5,10 +5,10 @@ class Solution:
     # Here, n is the length of the input string.
     # Time complexity: O(n)
     # The process involves iterating through the string once,
-    # from the first character to the second-last character, making it a linear iteration over n−1 indices
+    # from the first character to the second-last character, making it a linear iteration over n-1 indices
     # At each index, calculating the absolute difference
     # between the ASCII values of two adjacent characters requires constant time.
-    # Hence, the total time complexity for this operation is O(n−1)=O(n).
+    # Hence, the total time complexity for this operation is O(n-1)=O(n).
 
     # Space Complexity: O(1)
     # We only used a single additional variable, score, to accumulate the result.

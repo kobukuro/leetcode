e02a7d880afdc369ed818ea13828e828f977738a
Peter Chen <sanpeter992000@gmail.com>
2025-08-07 09:30:05 +0800

    Add search method for rotated sorted array and corresponding tests.

--- problems/medium/search_in_rotated_sorted_array_33/solution.py
+++ problems/medium/search_in_rotated_sorted_array_33/solution.py
@@ -1,8 +1,11 @@
-# Array, Binary Search
-# Time:O(log n), Space:O(1)
+# Tags: Array, Binary Search
+from typing import List
+
+
 class Solution:
+    # Time complexity: O(log n)
+    # Space complexity: O(1)
     def search(self, nums: List[int], target: int) -> int:
-        # Time:O(log n), Space:O(1)
         def binary_search(left, right):
             while left <= right:
                 mid = left + (right - left) // 2
@@ -13,27 +16,29 @@ class Solution:
                 else:
                     left = mid + 1
             return -1
+
         def find_rotation_index():
             left = 0
             right = len(nums) - 1
             while left <= right:
                 mid = left + (right - left) // 2
-                if nums[mid] > nums[mid+1]:
+                if nums[mid] > nums[mid + 1]:
                     return mid + 1
-                if nums[mid] < nums[mid-1]:
+                if nums[mid] < nums[mid - 1]:
                     return mid
                 if nums[mid] > nums[0]:
                     left = mid + 1
                 else:
                     right = mid - 1
+
         if len(nums) == 1:
             return 0 if nums[0] == target else -1
         if nums[-1] > nums[0]:
-            return binary_search(0, len(nums)-1)
+            return binary_search(0, len(nums) - 1)
         rotation_index = find_rotation_index()
         if target == nums[0]:
             return 0
         elif target > nums[0]:
-            return binary_search(0, rotation_index-1)
+            return binary_search(0, rotation_index - 1)
         else:
-            return binary_search(rotation_index, len(nums)-1)
+            return binary_search(rotation_index, len(nums) - 1)

944684893847044534664aeeaea1455828714b17
Peter Chen <sanpeter992000@gmail.com>
2025-08-09 09:15:32 +0800

    Change while loop condition from <= to < in reverse_string.

--- problems/easy/reverse_string_344/solution.py
+++ problems/easy/reverse_string_344/solution.py
@@ -8,7 +8,7 @@ class Solution:
     def reverse_string(self, s: List[str]) -> None:
         l = 0
         r = len(s) - 1
-        while l <= r:
+        while l < r:
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1

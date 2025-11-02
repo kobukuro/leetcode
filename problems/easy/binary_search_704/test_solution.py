a1f2facd4faaf4f4ce374d600243f66e87193c25
Peter Chen <sanpeter992000@gmail.com>
2025-11-02 11:06:03 +0800

    Remove extra space in parametrize decorator.

--- problems/easy/binary_search_704/test_solution.py
+++ problems/easy/binary_search_704/test_solution.py
@@ -6,7 +6,7 @@ from .solution import Solution
 class TestSolution(BaseTestSolution):
     solution = Solution()
 
-    @pytest.mark.parametrize("method_name, nums, target , expected, timeout", [
+    @pytest.mark.parametrize("method_name, nums, target, expected, timeout", [
         ('search', [-1, 0, 3, 5, 9, 12], 9, 4, None),
         ('search', [-1, 0, 3, 5, 9, 12], 2, -1, None),
     ])
